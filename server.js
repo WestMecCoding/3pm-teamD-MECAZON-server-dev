dfee2099964c7d8861b5fb35a35034f2a4bde66a
Leo Marquez <lmarqu358@west-mec.org>
2024-12-10 15:41:59 -0700

    trying to connect to mongo

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+const fs = require('fs');
 const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
+if (fs.existsSync('.env')) {
+  require('dotenv').config();
+}
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
+
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(express.json());
+
+//
+
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the process if the connection fails
+  }
+}
+
+startServer();
 
 // Routes
 
@@ -23,7 +52,22 @@ app.get("/api/employees", (req, res) => {
   res.json(employees);
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+//find users endpoint
+app.get("/find/:database/:collection", async (req, res) => {
+  try {
+    const { database, collection } = req.params;
+    const db = client.db(database);
+    const documents = await db.collection(collection).find({}).toArray();
+    res.status(200).json(documents);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
+
+
+
+// Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
