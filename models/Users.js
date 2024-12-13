43cf43945d5ae9785ff71c56e47928fc6ee7bbe5
Gabriel Gimnang <ggimna499@west-mec.org>
2024-12-12 17:30:28 -0700

    finished user schema

--- models/Users.js
+++ models/Users.js
@@ -15,6 +15,28 @@ const userSchema = new mongoose.Schema({
             trim: true
         },
     },
+    location: {
+        country: {
+            type: String,
+            required: [true, 'Country is required'],
+            trim: true
+        },
+        city: {
+            type: String,
+            required: [true, 'City is required'],
+            trim: true
+        },
+        address: {
+            type: String,
+            required: [true, 'Address is required'],
+            trim: true
+        },
+        zip_code: {
+            type: Number,
+            required: [true, 'Zip Code is required'],
+            trim: true
+        },
+    },
     contact_info: {
         email: {
             type: String,
@@ -27,19 +49,23 @@ const userSchema = new mongoose.Schema({
             required: [true, 'Phone number is required'],
             trim: true
         },
-        address: {
-            type: String,
-            required: [true, 'Address is required'],
-            trim: true
-        }
     },
     password: {
         type: String,
         required: [true, 'password is required'],
         trim: true
+    },
+    orders: {
+        type: Array,
+        required: [true, 'Orders is required'],
+        trim: true
+    },
+    payment_type: {
+        type: String,
+        required: [true, 'Payment Type is required']
     }
 
-    // Add other fields as necessary, no clue what to add now. Add Payment (String), Orders (Arr), Location (Rechange)
+    // Add other fields as necessary, no clue what to add now. Add Payment (String), Location (Rechange)
 }, {
     timestamps: true // Automatically adds createdAt and updatedAt fields
 });
